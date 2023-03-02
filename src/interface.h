5e88d16ec96d4e114a223ba7d0b99788e5d8f6b0
Lisa Shymkovich <95053643+LisaShymkovich@users.noreply.github.com>
2023-03-02 21:25:56 +0000

    Updated interface.h and server.c with "Proccess" function

--- src/interface.h
+++ src/interface.h
@@ -5,6 +5,8 @@
 #ifndef interface_h
 #define interface_h
 
+void Proccess(int* First_Class, int* Second_Class, const int Class_Size);
+
 int Compare(const void *First_Class, const void *Second_Class);
 
 #endif /* interface_h */
