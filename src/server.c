109c8a9b40cba7c72de619067ed9d5d75a05399c
Lisa Shymkovich <95053643+LisaShymkovich@users.noreply.github.com>
2023-03-03 18:02:31 +0000

    Added comments to every function and made some minor changes in code

--- src/server.c
+++ src/server.c
@@ -13,61 +13,61 @@ int Compare(const void *a, const void *b)
     return *(int*)a - *(int*)b;
 }
 
+
 int seed = 0;
-int*  Random_Class_Height(const int ARRAY_SIZE)
+int*  Random_Class_Height(const int Class_Size)
 {
-    int* my_array = (int*) malloc(ARRAY_SIZE * sizeof(int));
+    int* class = (int*) malloc(Class_Size * sizeof(int));
     if (seed == 0)
     {
         seed = time(NULL);
         srand(seed);
     }
-    for (int i = 0; i < ARRAY_SIZE; i++)
+    for (int i = 0; i < Class_Size; i++)
     {
-        my_array[i] = Randomizer(150, 200);
+        class[i] = Randomizer(150, 200);
     }
-    return my_array;
+    return class;
 }
-
 
 int Randomizer (int min, int max)
 {
     return rand()%(max-min+1) + min;
 }
 
-void Proccess(int* array1, int* array2, const int ARRAY_SIZE)
+
+void Proccess(int* class1, int* class2, const int Class_Size)
 {
     printf("Heigths of first class:\n");
-    for(int i = 0 ; i < ARRAY_SIZE; i++)
+    for(int i = 0 ; i < Class_Size; i++)
         {
             printf("%d. ", i+1);
-            printf("%d sm\n", array1[i]);
+            printf("%d sm\n", class1[i]);
         }
     
     printf("\n");
-    printf("Heigths of first class:\n");
-    for(int i = 0 ; i < ARRAY_SIZE; i++)
+    printf("Heigths of second class:\n");
+    for(int i = 0 ; i < Class_Size; i++)
     {
         printf("%d. ", i+1);
-        printf("%d sm\n", array2[i]);
+        printf("%d sm\n", class2[i]);
     }
     
     printf("\n");
     
     
-    if (Compare(&array1[ARRAY_SIZE-3], &array2[ARRAY_SIZE-3])>0)
+    if (Compare(&class1[Class_Size-3], &class2[Class_Size-3])>0)
     {
-        printf("The person in first class is taller ");
+        printf("The person in first class is taller \n");
     }
     
-    if (Compare(&array1[ARRAY_SIZE-3], &array2[ARRAY_SIZE-3])<0)
+    if (Compare(&class1[Class_Size-3], &class2[Class_Size-3])<0)
     {
-        printf("The person in second class is taller ");
+        printf("The person in second class is taller \n");
     }
     
-    if (Compare(&array1[ARRAY_SIZE-3], &array2[ARRAY_SIZE-3])==0)
+    if (Compare(&class1[Class_Size-3], &class2[Class_Size-3])==0)
     {
-        printf("The person in first class and in second are the same height");
+        printf("The person in first class and in second are the same height\n");
     }
 }
-
