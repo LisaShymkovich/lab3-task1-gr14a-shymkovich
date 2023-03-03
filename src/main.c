cbbda93cc4a7a572d665ce7ee2259a4d6b4b71ff
Lisa Shymkovich <95053643+LisaShymkovich@users.noreply.github.com>
2023-03-03 17:47:10 +0000

    Updated main with function call

--- src/main.c
+++ src/main.c
@@ -3,9 +3,18 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "interface.h"
 
 int main(int argc, const char * argv[])
 {
+    const int Class_Size=23;
+    int* First_Class = Random_Class_Height(Class_Size);
+    qsort(First_Class, Class_Size,sizeof(int), Compare);
+    int* Second_Class =  Random_Class_Height(Class_Size);
+    qsort(Second_Class, Class_Size, sizeof(int), Compare);
+    Proccess(First_Class, Second_Class, Class_Size);
   return 0;
 }
 
